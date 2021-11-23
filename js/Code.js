22a11e109bd226baacd2762c3939881b28fa8124
newMoon4578 <87790930+newMoon4578@users.noreply.github.com>
2021-11-22 18:07:39 -0800

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode3 = "FUNCTION";
+const accessCode2 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("REVBAILA", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("CUTNIFON", 100,250)
+    fill("lightblue")
+    text("Hint: Performs a particular task!", 100,270)
+
+    fill("white")
+    textSize(15)
+    text("ATEDASBA", 700,150)
+    fill("lightblue")
+    text("Hint: Stores all information!", 700,170)
+
+}
